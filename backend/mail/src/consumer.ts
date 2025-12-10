fc483a7a36cec12dbe80e2fc2d3897855a962369
i-OmSharma <sharmaom1201@gmail.com>
2025-12-10 22:57:11 +0530

    backend updated

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -7,6 +7,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
+    secure: true,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD
